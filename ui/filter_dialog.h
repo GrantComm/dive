fc20ff60f250ced7be20bc8b062d04ef40a550d4
Grant <gcommodore@google.com>
2024-08-28 21:35:29 +0000

    Adds filter button that triggers the filter dialog

---
+++ ui/filter_dialog.h
@@ -0,0 +1,49 @@
+/*
+ Copyright 2019 Google LLC
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+#include <QDialog>
+#include <unordered_set>
+
+#pragma once
+
+// Forward declarations
+class QCheckBox;
+class QPushButton;
+
+class FilterDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    FilterDialog(QWidget* parent = nullptr);
+
+public slots:
+    void selectAllEventsFilter(int state);
+    void selectFilter(int state);
+    void applyFilters();
+    void closeDialog();
+
+signals:
+
+private:
+    QCheckBox   *m_all_filter = nullptr;
+    QCheckBox   *m_bind_filter = nullptr;
+    QCheckBox   *m_clear_filter = nullptr;
+    QCheckBox   *m_copy_filter = nullptr;
+    QCheckBox   *m_draw_filter = nullptr;
+    QPushButton *m_apply = nullptr;
+    
+    std::unordered_set<QCheckBox*> m_active_filters;
+    std::unordered_set<QCheckBox*> m_filters;
+    bool m_submitted;
+};
