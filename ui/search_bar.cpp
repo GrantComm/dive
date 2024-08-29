73d52b5b2b4bf7b00d38bc609ab70dc8cea4143b
Grant <gcommodore@google.com>
2024-08-29 23:17:44 +0000

    Adds cancel search button

--- ui/search_bar.cpp
+++ ui/search_bar.cpp
@@ -42,6 +42,9 @@ SearchBar::SearchBar(QWidget* parent) :
 
     m_next = new QPushButton(this);
     m_next->setIcon(QIcon(":/images/arrow_down.png"));
+    
+    m_cancel = new QPushButton(this);
+    m_cancel->setIcon(QIcon(":/images/cancel.png"));
 
     m_cancel = new QPushButton(this);
     m_cancel->setIcon(QIcon(":/images/cancel.png"));
