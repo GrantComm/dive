74083b5a9a9e36e2779c93b295507ec78836d008
Grant <gcommodore@google.com>
2025-07-17 18:56:12 +0000

    Removes stale gfxr directory

--- third_party/gfxreconstruct/android/tools/replay/.cxx/Debug/6d501z3k/arm64-v8a/abseil-cpp/absl/base/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/local/google/home/gcommodore/dive/third_party/abseil-cpp/absl/base
-# Build directory: /usr/local/google/home/gcommodore/dive/third_party/gfxreconstruct/android/tools/replay/.cxx/Debug/6d501z3k/arm64-v8a/abseil-cpp/absl/base
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
